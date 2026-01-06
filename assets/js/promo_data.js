3b494accfa3b8c54090d3cbe363b2afd659d8f79
USS Admin <admin@usstech.net>
2026-01-06 16:02:55 -0600

    feat(store): Import real promotional banner from LTS (force add)
    - Scraped high-quality homepage banner from LTS site.
    - Mapped all store categories to this active promotion.

--- assets/js/promo_data.js
+++ assets/js/promo_data.js
@@ -1,8 +1,9 @@
 const promoData = {
-    "platinum": "assets/images/promos/promo_platinum.jpg",
-    "pro": "assets/images/promos/promo_pro.jpg",
-    "camera": "assets/images/promos/promo_camera.jpg",
-    "nvr": "assets/images/promos/promo_nvr.jpg",
-    "access": "assets/images/promos/promo_access.jpg",
+    "platinum": "assets/images/promos/promo_default.jpg",
+    "pro": "assets/images/promos/promo_default.jpg",
+    "camera": "assets/images/promos/promo_default.jpg",
+    "nvr": "assets/images/promos/promo_default.jpg",
+    "access": "assets/images/promos/promo_default.jpg",
+    "ubiquiti": "assets/images/promos/promo_default.jpg",
     "default": "assets/images/promos/promo_default.jpg"
 };
