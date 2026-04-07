e9198ddcfb42d17dc8344c63598ff670eb2489d0
NateFoxtrot <nathan.s.follis@gmail.com>
2026-04-07 14:22:27 -0500

    Remove base path for custom domain root serving, fix APK download link

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://usstech.net',
-  base: '/ultimate-security-solutions-site/',
   trailingSlash: 'never',
   integrations: [sitemap()],
 });
